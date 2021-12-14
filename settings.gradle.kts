103d72ec77161238eee296dcc7d10afe7f4beff2
weaselflink <stefan@stefanbissell.de>
2021-12-14 11:00:26 +0100

    keep dependencies up to date

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
     val kotlin_version: String by settings
     plugins {
         kotlin("jvm") version kotlin_version
-        id("com.github.ben-manes.versions") version "0.36.0"
-        id("com.github.johnrengelman.shadow") version "6.1.0"
+        id("com.github.ben-manes.versions") version "0.39.0"
+        id("com.github.johnrengelman.shadow") version "7.1.0"
     }
 }
