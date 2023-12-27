da209e16f438f7e92864cac6aef55b9bd6f2c19e
weaselflink <stefan@stefanbissell.de>
2023-12-27 09:41:46 +0100

    keep dependencies up to date

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,9 @@
-@file:Suppress("SuspiciousCollectionReassignment")
-
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     application
-    kotlin("jvm")
-    id("com.github.ben-manes.versions")
-    id("com.github.johnrengelman.shadow")
+    kotlin("jvm") version "1.9.22"
+    id("com.github.ben-manes.versions") version "0.50.0"
+    id("se.ascp.gradle.gradle-versions-filter") version "0.1.16"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 repositories {
@@ -15,23 +12,13 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
 }
 
 application {
-    @Suppress("DEPRECATION")
     mainClass.set("de.stefanbissell.easyclock.AppKt")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-
-tasks {
-    withType<KotlinCompile>().configureEach {
-        kotlinOptions {
-            jvmTarget = "11"
-        }
-    }
+kotlin {
+    jvmToolchain(11)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,1 @@
-@file:Suppress("LocalVariableName")
-
 rootProject.name = "easy-clock"
-
-pluginManagement {
-    val kotlin_version: String by settings
-    plugins {
-        kotlin("jvm") version kotlin_version
-        id("com.github.ben-manes.versions") version "0.39.0"
-        id("com.github.johnrengelman.shadow") version "7.1.0"
-    }
-}
